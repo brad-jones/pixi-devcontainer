41dc71b4cd63c52da247ba90afd7bb8bf926e18d
Brad Jones <brad@bjc.id.au>
2023-12-27 20:55:57 +1100

    chore: getting ready for first publish

--- src/pixi/install.sh
+++ src/pixi/install.sh
@@ -1,24 +1,42 @@
-#!/bin/sh
+#!/usr/bin/bash
 set -e
 
-build_url() {
-  echo "https://github.com/prefix-dev/pixi/releases/${VERSION:-latest}/download/pixi-x86_64-unknown-linux-musl.tar.gz"
-}
+# OPTIONS
+# ------------------------------------------------------------------------------
+PIXI_VERSION="${VERSION:-"latest"}"
+PIXI_ADD_PATH_SHELLRC_FILES="${ADDPATHTOSHELLRCFILES:-true}"
+PIXI_DOWNLOAD_URL="https://github.com/prefix-dev/pixi/releases/${PIXI_VERSION}/download/pixi-x86_64-unknown-linux-musl.tar.gz"
 
+# Create a simple wrapper for pixi that modifies XDG_CACHE_HOME but just for
+# pixi we we don't want to effect any other tooling installed in the devcontainer
+# which may have it's own cache volumes setup.
+#
+# The only reason we need to do this is it is currently not possible to create
+# a mount target with a variable. ie: the ${remoteUser} or their home dir.
+# ------------------------------------------------------------------------------
 install_pixi_wrapper() {
-  cat <<-"EOF" >>/usr/bin/pixi
-#!/usr/bin/env bash
+  cat <<"EOF" >>/usr/bin/pixi
+#!/usr/bin/bash
+set -e
 export XDG_CACHE_HOME="/pixi-cache"
 exec /opt/pixi "$@"
 EOF
   chmod +x /usr/bin/pixi
 }
 
+# Install the script that corrects volume mount permissions & adds pixi PATHs
+# It runs whenever a new devcontainer is created for the first time.
+# ------------------------------------------------------------------------------
 install_pixi_onCreateCommand() {
-  cat <<-"EOF" >>/usr/local/share/pixi-onCreateCommand.sh
+  cat <<EOF >>/usr/local/share/pixi-onCreateCommand.sh
 #!/bin/sh
 set -e
+PIXI_ADD_PATH_SHELLRC_FILES="$PIXI_ADD_PATH_SHELLRC_FILES"
+EOF
 
+  cat <<-"EOF" >>/usr/local/share/pixi-onCreateCommand.sh
+# Mounts are created by root so this is our best effort to set permissions
+# for the current user so they can write to the volumes.
 if [ -n "$USER" ] && [ "$USER" != "root" ]; then
   if command -v sudo >/dev/null; then
     sudo chown -R ${USER} ./.pixi /pixi-cache
@@ -27,17 +45,26 @@ if [ -n "$USER" ] && [ "$USER" != "root" ]; then
   fi
 fi
 
-echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.bashrc"
-echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.bashrc"
-echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.zshrc"
-echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.zshrc"
-echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.profile"
-echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.profile"
+if [ "$PIXI_ADD_PATH_SHELLRC_FILES" = true ]; then
+  # Make sure globally installed tools are on PATH
+  echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.bashrc"
+  echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.zshrc"
+  echo "export PATH=\"\$HOME/.pixi/bin:\$PATH\"" >> "$HOME/.profile"
+
+  # Make sure locally installed tools are on PATH
+  echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.bashrc"
+  echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.zshrc"
+  echo "export PATH=\"$PWD/.pixi/env/bin:\$PATH\"" >> "$HOME/.profile"
+fi
 EOF
 
   chmod +x /usr/local/share/pixi-onCreateCommand.sh
 }
 
+# Installs the script that either executes pixi install or pixi init when the
+# container starts & when it re-starts and the workspace content has changed
+# since the last start.
+# ------------------------------------------------------------------------------
 install_pixi_updateContentCommand() {
   cat <<-"EOF" >>/usr/local/share/pixi-updateContentCommand.sh
 #!/bin/sh
@@ -53,15 +80,19 @@ EOF
   chmod +x /usr/local/share/pixi-updateContentCommand.sh
 }
 
+# This actually does the instalation of the pixi command it's self
+# ------------------------------------------------------------------------------
 install_pixi() {
   mkdir -p /tmp/pixi
-  curl -L "$(build_url)" -o /tmp/pixi.tar.gz
+  curl -L "${PIXI_DOWNLOAD_URL}" -o /tmp/pixi.tar.gz
   tar -xf /tmp/pixi.tar.gz -C /tmp/pixi
   chmod +x /tmp/pixi/pixi
   mv /tmp/pixi/pixi /opt/pixi
   rm -rf /tmp/pixi /tmp/pixi.tar.gz
 }
 
+# The following installs & then uninstall curl if need be
+# ------------------------------------------------------------------------------
 installed_curl=false
 
 ensure_curl() {
@@ -85,6 +116,8 @@ clean_curl() {
   fi
 }
 
+# The following installs & then uninstall tar if need be
+# ------------------------------------------------------------------------------
 installed_tar=false
 
 ensure_tar() {
@@ -108,6 +141,8 @@ clean_tar() {
   fi
 }
 
+# The main entrypoint of the script
+# ------------------------------------------------------------------------------
 main() {
   ensure_tar
   ensure_curl
